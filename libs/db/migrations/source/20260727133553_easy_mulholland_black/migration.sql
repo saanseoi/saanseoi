6c4906edd1d4f74c7c62029747428fd611fd5fe3
Mart van de Ven <m@type.hk>
2026-07-27 21:36:34 +0800

    refactor(db): move source crs to dataset metadata

--- libs/db/migrations/source/20260727133553_easy_mulholland_black/migration.sql
+++ libs/db/migrations/source/20260727133553_easy_mulholland_black/migration.sql
@@ -1,4 +1,3 @@
-ALTER TABLE `hkgovCenstatdDivisionAreas` ADD `sourceCrs` text NOT NULL;--> statement-breakpoint
 ALTER TABLE `hkgovHadDivisionAreas` DROP COLUMN `sourceCrs`;--> statement-breakpoint
 ALTER TABLE `hkgovCenstatdDistrictLandAreaPopulationDensities` DROP COLUMN `sourceCrs`;--> statement-breakpoint
 ALTER TABLE `hkgovPlandDivisionAreas` DROP COLUMN `sourceCrs`;--> statement-breakpoint
