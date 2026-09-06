99c4c0b2b8426d90bec1af24490abf4ba5f654ca
Mart van de Ven <m@type.hk>
2026-09-06 12:24:50 +0800

    refactor(harbour-cli): modularise street uploads

---
+++ apps/harbour-cli/src/lib/streetSql/processLocalStreetSqlUploadConfig.ts
@@ -0,0 +1,7 @@
+import { streetLocaleCodes } from '@repo/db'
+
+export const LOCAL_RELEASE_ROOT = `${import.meta.dir}/../../../../../.local/harbour-sql/releases`
+
+export const LANDSD_STREET_SNAPSHOT_SOURCE_ROLE = 'primary'
+
+export const REQUIRED_LOCALES = streetLocaleCodes
