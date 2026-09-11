1907a0b8e5c77bd432083c77d696f22748c5579c
Mart van de Ven <m@type.hk>
2026-09-11 16:22:16 +0800

    fix(db): rename sources columns to sourceLocator

---
+++ libs/db/migrations/source/20260910184445_typical_alex_power/migration.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `overtureDivisionAreas` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `overtureDivisionBoundaries` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `overtureDivisions` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `overturePlaces` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `hkgovAlsAddresses2d` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `hkgovAlsAddresses3d` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `hkgovLandsdPlaceNames` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `hkgovHadDivisionAreas` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `hkgovCenstatdDistrictLandAreaPopulationDensities` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `hkgovCenstatdDivisionAreas` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `hkgovCenstatdStatistics` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `hkgovPlandNewTowns` RENAME COLUMN `sources` TO `sourceLocator`;--> statement-breakpoint
+ALTER TABLE `hkgovPlandPlanningCells` RENAME COLUMN `sources` TO `sourceLocator`;
